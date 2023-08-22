e6ae2516fd0e8c1a9337d92c1bfac5f0fb660823
Emmalcd <joselemmanuel9@gmail.com>
2023-08-22 17:03:15 -0600

    Se llego hasta funciones con parámetros

---
+++ funciones.js
@@ -0,0 +1,14 @@
+function saltarlinea(){
+    document.write("<br>");
+}
+var year = 2025;
+var saltolinea="<br><br>"
+
+document.write("Juan tiene: "+(year-2000)+" años");
+saltarlinea();
+saltarlinea();
+document.write("Pedro tiene: "+(year-1995)+" años");
+document.write(saltolinea);
+document.write("Carlos tiene: "+(year-2005)+" años");
+document.write("<br>");
+document.write("<br>");
---
+++ variables.js
@@ -0,0 +1,12 @@
+
+var year = 2025;
+var saltolinea="<br><br>"
+
+document.write("Juan tiene: "+(year-2000)+" años");
+document.write("<br>");
+document.write("<br>");
+document.write("Pedro tiene: "+(year-1995)+" años");
+document.write(saltolinea);
+document.write("Carlos tiene: "+(year-2005)+" años");
+document.write("<br>");
+document.write("<br>");
